7b186ff951954111b956fc7185c94daf320ff424
Delilah Lopez <Delilah.marinaL@gmail.com>
2025-12-04 15:51:13 -0500

    created method for get request in order to get allUsers

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -1,15 +1,16 @@
 import User from "../models/UserModel.ts"
-import { Request,Response,NextFunction,Error } from "express"
+import { Request,Response,NextFunction } from "express"
 
 const UserController = {
 
 createUser :async (req: Request, res : Response, next: NextFunction) => {
     try{
+      console.log(req.body)
         const newUser = await User.create(req.body);
         res.locals.newUser = newUser; 
         return next()
 
-    } catch(err: Error){
+    } catch(error: unknown){
     return next(
         {
           log:`Something went wrong adding a new user`,
@@ -20,7 +21,27 @@ createUser :async (req: Request, res : Response, next: NextFunction) => {
        )
     }
  
-}   
+},
+
+getAllUsers: async(req : Request, res: Response, next : NextFunction) => {
+    try{
+      const findAllUsers = await User.find();
+
+     res.locals.allUsers = findAllUsers;
+      
+      return next()
+      
+    }catch(error : unknown){
+      return next(
+        {
+          log:`Something went wrong finding User`,
+          status: 500,
+          message:{ error :`Sorry, we cannot find those User.`}
+          
+        }
+      )
+    }
+  },
 }
 
 export default UserController;
